dbb01a76ab7ae2604ee04f769e761dd46d1bb469
Jerry Liu <zhengyu.liu@duke.edu>
2017-10-14 07:11:54 -0400

    shitty scrolling

--- js/contentscript.js
+++ js/contentscript.js
@@ -34,3 +34,38 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 function goBackToPreviousPage(){
     window.history.back();
 }
+
+function scrollYDistance(targetY, duration, onDone) {
+			stopScroll()
+			if (duration === 0 || (duration && duration < 0) || isNativeSmoothScrollEnabledOn(container.body)) {
+				container.toY(targetY)
+				if (onDone) {
+					onDone()
+				}
+			} else {
+				var startY = container.getY()
+				var distance = Math.max(0, targetY) - startY
+				var startTime = new Date().getTime()
+				duration = duration || Math.min(Math.abs(distance), defaultDuration);
+				(function loopScroll() {
+					setScrollTimeoutId(setTimeout(function () {
+						// Calculate percentage:
+						var p = Math.min(1, (new Date().getTime() - startTime) / duration)
+						// Calculate the absolute vertical position:
+						var y = Math.max(0, Math.floor(startY + distance*(p < 0.5 ? 2*p*p : p*(4 - p*2)-1)))
+						container.toY(y)
+						if (p < 1 && (container.getHeight() + y) < container.body.scrollHeight) {
+							loopScroll()
+						} else {
+							setTimeout(stopScroll, 99) // with cooldown time
+							if (onDone) {
+								onDone()
+							}
+						}
+					}, 9))
+				})()
+			}
+		}
+setTimeout(function () {
+    scroll;
+}, 5000);
