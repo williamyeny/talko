d08ba688f3a2d18f28c92c57b7e8fc544cce3210
williamyeny <williamyeny@gmail.com>
2017-10-14 02:56:21 -0400

    added bing client support

---
+++ js/backgroundscript.js
@@ -0,0 +1,5 @@
+var bingClientRecognition = new BingSpeech.RecognitionClient("2bfaacabb8314b7bad94b469f1079a81"); // haha totally not a secret thing
+
+bingClientRecognition.startMicAndContinuousRecognition();
+
+bingClientRecognition.endMicAndContinuousRecognition();
